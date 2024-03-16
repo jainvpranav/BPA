00926faa1e07cbab82ee08c2f5dd6e34109cdf55
Pranav <pranavv2002@gmail.com>
2024-03-16 21:17:41 +0530

    Index Bugs fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const nav1 = document.getElementById("nav1");
 const nav2 = document.getElementById("nav2");
 const hamburger = document.getElementById("hamburger");
+const hamimg = document.getElementById("ham-img")
 if(screen.width < 850) {
     hamburger.style.display="block";
     nav1.style.display="none";
@@ -9,6 +10,10 @@ if(screen.width < 850) {
 hamburger.addEventListener("click", () => {
     if(nav2.style.display === "flex") {
         nav2.style.display = "none";
+        hamimg.src="./assets/vectors/landing/menu.png";
     }
-    else nav2.style.display="flex";
+    else {
+        nav2.style.display="flex";
+        hamimg.src="./assets/vectors/landing/close.png";
+    }
 })
