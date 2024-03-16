fc430eeccad28116f1f863cb1267f72332c8d5e1
Pranav <pranavv2002@gmail.com>
2024-03-16 21:40:30 +0530

    Some changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,18 @@ const nav1 = document.getElementById("nav1");
 const nav2 = document.getElementById("nav2");
 const hamburger = document.getElementById("hamburger");
 const hamimg = document.getElementById("ham-img")
-if(screen.width < 850) {
-    hamburger.style.display="block";
-    nav1.style.display="none";
+if (screen.width < 850) {
+    hamburger.style.display = "block";
+    nav1.style.display = "none";
     // nav2.style.display="flex";
 }
 hamburger.addEventListener("click", () => {
-    if(nav2.style.display === "flex") {
+    if (nav2.style.display === "flex") {
         nav2.style.display = "none";
-        hamimg.src="./assets/vectors/landing/menu.png";
+        hamimg.src = "./assets/vectors/landing/menu.png";
     }
     else {
-        nav2.style.display="flex";
-        hamimg.src="./assets/vectors/landing/close.png";
+        nav2.style.display = "flex";
+        hamimg.src = "./assets/vectors/landing/close.png";
     }
 })
